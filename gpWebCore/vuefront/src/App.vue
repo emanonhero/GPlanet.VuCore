eaf3f51b042fe2af48eee5dec12a286c973dd4d0
emanonhero <emanonhero@qq.com>
2019-07-12 14:52:36 +0800

    kuohan

--- gpWebCore/vuefront/src/App.vue
+++ gpWebCore/vuefront/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="你好 Welcome to Your Vue.js App"/>
   </div>
 </template>
 
